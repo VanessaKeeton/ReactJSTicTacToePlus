d16ae98fbe676289dad02295f5cb956cca4851e1
Vanessa Keeton <vanessakeeton@Vanessas-MacBook-Pro.local>
2018-07-24 00:51:02 -0400

    Adjusting tabs to match the two space standard from init react files

--- src/index.js
+++ src/index.js
@@ -17,26 +17,26 @@ function Square(props) {
 let matrix = 3;
 
 class Board extends React.Component {
-   renderRows(numRows,numCols) {
-	let rows = [];
-	let numSquares = 0;
-	for (let i = 0; i < numRows; i++) {
-		numSquares = (numCols * i)
-		let cols = [];
-		for (let j = numSquares ;j < numCols + numSquares; j++) {
-			console.log(this.props.winner.winningCombination);
-       		cols.push(
-       			<Square
-			        value={this.props.squares[j]}
-			        key={j}
-			        onClick={() => this.props.onClick(j)}
-			        className = {this.props.winner.winningCombination.includes(j) ? 'winning-square square' : 'square'}
-		      	/>
-		      );
+  renderRows(numRows,numCols) {
+    let rows = [];
+    let numSquares = 0;
+    for (let i = 0; i < numRows; i++) {
+      numSquares = (numCols * i)
+      let cols = [];
+      for (let j = numSquares ;j < numCols + numSquares; j++) {
+        console.log(this.props.winner.winningCombination);
+          cols.push(
+            <Square
+              value={this.props.squares[j]}
+              key={j}
+              onClick={() => this.props.onClick(j)}
+              className = {this.props.winner.winningCombination.includes(j) ? 'winning-square square' : 'square'}
+            />
+          );
       }
-      rows.push(<div key={ i } className="board-row">{cols}</div>);
-	}
-	return rows;
+    rows.push(<div key={ i } className="board-row">{cols}</div>);
+  }
+  return rows;
 }
 
   render() {
@@ -64,11 +64,11 @@ class Game extends React.Component {
   }
 
   componentWillMount() {
-	    this.initialState = this.state
-	}
+  this.initialState = this.state
+  }
   
   reset() {
-  	this.setState(this.initialState);
+    this.setState(this.initialState);
   }
   
 
@@ -103,16 +103,17 @@ class Game extends React.Component {
       stepNumber: step,
       xIsNext: (step % 2) === 0,
       activeIndex: step
-    	});
-	}
-toggle() {
-    	this.setState({
-         orderAssending: !this.state.orderAssending
-       });
-    }
+      });
+  }
+
+  toggle() {
+    this.setState({
+       orderAssending: !this.state.orderAssending
+     });
+  }
 
   render() {
-  	const activeIndex = this.state.activeIndex;
+    const activeIndex = this.state.activeIndex;
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares,matrix);
@@ -129,18 +130,18 @@ toggle() {
     });
 
     if(!this.state.orderAssending) { 
-    	moves.reverse();
+      moves.reverse();
     }
 
     let status;
     if (winner.winner !== '') {
       status = "Winner: " + winner.winner;
     } else {
-    	if (current.squares.includes(null)) {
-      		status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-  		}else{
-  			status = "This game is a draw."
-  		}
+      if (current.squares.includes(null)) {
+          status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+      }else{
+        status = "This game is a draw."
+      }
     }
 
     return (
@@ -168,79 +169,79 @@ toggle() {
 ReactDOM.render(<Game />, document.getElementById("root"));
 
 function calculateWinner(squares,matrix) {
-	// set winning combinations
-	let winningCombinations = [];
-	for (let i = 0; i < (matrix * 2) + 2; i++) {
-		winningCombinations.push([]);
-	}
-	let jStore = 0;
-	let currentIndex;
+  // set winning combinations
+  let winningCombinations = [];
+  for (let i = 0; i < (matrix * 2) + 2; i++) {
+    winningCombinations.push([]);
+  }
+  let jStore = 0;
+  let currentIndex;
 
-	//across cols win
-	for (let i = 0; i < matrix; i++) {
-		for (let j = 0; j < matrix; j++) {
-			currentIndex = j + jStore;
-			winningCombinations[i].push(currentIndex);
-			if (j === matrix - 1) {
-				jStore = j + jStore + 1 ;
-			};
-		}
-	}
+  //across cols win
+  for (let i = 0; i < matrix; i++) {
+    for (let j = 0; j < matrix; j++) {
+      currentIndex = j + jStore;
+      winningCombinations[i].push(currentIndex);
+      if (j === matrix - 1) {
+        jStore = j + jStore + 1 ;
+      };
+    }
+  }
 
-	//down rows win
-	for (let i = 0; i < matrix; i++) {
-		currentIndex = i;
-		for (let j = 0; j < matrix; j++) {
-			if (j !== 0) {
-				currentIndex = currentIndex + matrix;
-			}
-			winningCombinations[i + matrix].push(currentIndex);
-		}
-	}
+  //down rows win
+  for (let i = 0; i < matrix; i++) {
+    currentIndex = i;
+    for (let j = 0; j < matrix; j++) {
+      if (j !== 0) {
+        currentIndex = currentIndex + matrix;
+      }
+      winningCombinations[i + matrix].push(currentIndex);
+    }
+  }
 
-	// diaginal win 1 ;
-	let sqaureIndex = 0;
-	for (let i = 0; i < matrix; i++) {
-		winningCombinations[matrix + matrix].push(sqaureIndex);
-		sqaureIndex = sqaureIndex + matrix + 1;
-	}
+  // diaginal win 1 ;
+  let sqaureIndex = 0;
+  for (let i = 0; i < matrix; i++) {
+    winningCombinations[matrix + matrix].push(sqaureIndex);
+    sqaureIndex = sqaureIndex + matrix + 1;
+  }
 
-	// diaginal win 2
-	sqaureIndex = matrix - 1;
-	for (let i = 0; i < matrix; i++) {
-		winningCombinations[matrix + matrix + 1].push(sqaureIndex);
-		sqaureIndex = sqaureIndex + matrix - 1;
-	}
+  // diaginal win 2
+  sqaureIndex = matrix - 1;
+  for (let i = 0; i < matrix; i++) {
+    winningCombinations[matrix + matrix + 1].push(sqaureIndex);
+    sqaureIndex = sqaureIndex + matrix - 1;
+  }
 
 //test for winning combination
 for (let i = 0; i < winningCombinations.length; i++) {
-	let combination = [];
+  let combination = [];
     winningCombinations[i].forEach(function (square, index){
-    		combination.push(squares[square]);
-    	});
+        combination.push(squares[square]);
+      });
 
     if (AllValuesSameButNotNull(combination)) {
       return {
-      	winner : combination[0], 
-      	winningCombination : winningCombinations[i]
+        winner : combination[0], 
+        winningCombination : winningCombinations[i]
       };
     }
   }
   return {
-      	winner : '', 
-      	winningCombination : ''
+        winner : '', 
+        winningCombination : ''
       };
 }
 
 function AllValuesSameButNotNull(array){
  
-    if(array.length > 0) {
-        for(var i = 1; i < array.length; i++)
-        {
-        	if (array[i] === null || array[i] !== array[0]) {
-        		return false;
-        	}
-        }
-    } 
-    return true;
+  if(array.length > 0) {
+    for(var i = 1; i < array.length; i++)
+    {
+      if (array[i] === null || array[i] !== array[0]) {
+        return false;
+      }
+    }
+  } 
+  return true;
 }
